0f1861b924f1a59cc7ebd1806ab9359df53c8b13
Santiagogalle <santiagogalleguillo2005@gmail.com>
2023-06-29 14:51:01 -0300

    decimosexta revision de htmls y clases

--- Valorantinicio/views.py
+++ Valorantinicio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import CrearProfesionalesForm
+from .forms import CrearProfesionalesForm, JugadoresvalorantForm
 
 # Create your views here.
 
@@ -12,9 +12,6 @@ def crear_Profesionales(request):
     form = CrearProfesionalesForm()
     return render(request, 'Valorantinicio/crearProfesionales.html', {'form': form})
 
-from django.shortcuts import render
-from .forms import JugadoresvalorantForm
-
-def Jugadoresvalorant(request):
+def Jugadores_valorant(request):
     form = JugadoresvalorantForm()
     return render(request, 'Valorantinicio/Jugadoresvalorant.html', {'form': form})
