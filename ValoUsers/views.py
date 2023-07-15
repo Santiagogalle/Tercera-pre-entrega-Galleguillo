27aaaccdf9651e47ef0074a09e9c361eebfc69ce
Santiagogalle <santiagogalleguillo2005@gmail.com>
2023-07-15 01:20:13 -0300

    terminamos login arrancamos logout, nos pausamos aca debido a inconvenientes con la creacion de usuario

--- ValoUsers/views.py
+++ ValoUsers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as django_login
 
 # Create your views here.
 
@@ -7,10 +8,16 @@ def login(request):
 
    if request.method == 'POST':
       if form.is_valid():
-         ...
+        usuario = form.cleaned_data['username']
+        contrasenia = form.cleaned_data['password']
+
+        user = authenticate(username=usuario, password=contrasenia)
+
+        django_login(request, user)
+        return redirect('Valorantinicio:Valorantinicio')
+       
       else:
        return render(request, 'ValoUsers/login.html', {'form': form})
-
 
    form = AuthenticationForm()
    return render(request, 'ValoUsers/login.html', {'form': form})
