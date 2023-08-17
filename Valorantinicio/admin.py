cf572630321e25c86b96932b726e6f83e1217110
Santiagogalle <santiagogalleguillo2005@gmail.com>
2023-08-17 19:33:15 -0300

    agregue un par de comentarios en el readme y si no era la primera opcion registre los usuarios/modelos en el apartado de admin.py

--- Valorantinicio/admin.py
+++ Valorantinicio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# Lautaro kkk@kkk.pepsi lautaro galle
+# MarianaDemusso
+# adminregistrado
+# santiago pepito123@pepi.123
+# santiagogalleguillo	santiagogalleguillo2005@gmail.com
+# (como dije en el read.me hay algunos usuarios que use de ejemplo por ende no contienen todos los campos ya que fueron creados previos al paso de edicion de usuarios y de los usuarios que aparecen aca el orden de sus camos son(username,email,nombre y apellido))
